e1c1ee7388cdb36f0d824eadca43e0b562cf3338
Aryuko <pachimaryu@gmail.com>
2023-11-07 17:20:56 +0100

    rework socket structure to a list

--- src/backend/app.js
+++ src/backend/app.js
@@ -51,11 +51,33 @@ chatClient.onJoin((channel, user) => {
         // console.log(channelData);
     }
 })
+chatClient.onMessage(async (channel, user, message, msg) => {
+    if (channelData[channel]) {
+        // User messages
+        if (channelData[channel].gameState == gameStates.Active && message.includes("🦞")) {
+            addLobster(channel, user, msg.userInfo.color)
+        } // Mod commands
+        else if (msg.userInfo.isMod || msg.userInfo.isBroadcaster) {
+            switch (message.trim()) {
+                case "!startrace":
+                    startRace(channel)
+                    break
+                case "!stoprace":
+                    stopRace(channel)
+                    break
+                case "!mockdata":
+                    startMockData(channel, 100)
+                    break
+            }
+        }
+    }
+})
 
 /* CLIENT CONNECTION */
 var clientCount = 0
 var channelData = {}
 var channelDataIntervals = {}
+var channelClients = {}
 const gameStates = {
     Inactive: "inactive",
     Active: "active",
@@ -63,21 +85,37 @@ const gameStates = {
 }
 io.on('connection', (socket) => {
     console.log(`Client connected, serving ${++clientCount} total connection(s)`)
-    socket.on('disconnect', () => { console.log(`Client disconnected, serving ${--clientCount} total connection(s)`) })
-
+    socket.on('disconnect', () => {
+        // Remove socket from channelClients list
+        for (let c in channelClients) {
+            let i = channelClients[c].indexOf(socket)
+            if (i != -1) {
+                channelClients[c].splice(i, 1)
+            }
+        }
+        console.log(`Client disconnected, serving ${--clientCount} total connection(s)`)
+    })
 
     // TODO: leave channel if socket inactive
-    // TODO: sync lobster data on init if it already exists
+    // TODO: add socket id to list
     socket.on('init', (data) => {
         if (data && data.channel) {
-            console.log('init id:', socket.CLIENT_ID)
+            console.log('init id:', socket.id)
+            channelClients[data.channel] ??= []
+
+            if (!channelClients[data.channel].includes(socket)) {
+                channelClients[data.channel].push(socket)
+            }
+
             // TODO: Only join if you are not already part of the channel
             chatClient.join(data.channel)
+
             if (channelData[data.channel]) {
                 console.log("data exists already, syncing")
-                syncData(socket, data.channel)
+                syncData(data.channel, socket)
             } else {
                 channelData[data.channel] = {
+                    "clients": [],
                     "gameState": gameStates.Inactive,
                     "goal": data.config.goal,
                     "winner": null,
@@ -85,78 +123,65 @@ io.on('connection', (socket) => {
                 }
             }
 
-            socket.on('requestMockData', (data2) => {
-                startMockData(socket, data.channel, 100)
-            })
+            channelData[data.channel].clients
 
-            chatClient.onMessage(async (channel, user, message, msg) => {
-                if (channel == data.channel && channelData[channel]) {
-                    // User messages
-                    if (channelData[channel].gameState == gameStates.Active && message.includes("🦞")) {
-                        addLobster(socket, channel, user, msg.userInfo.color)
-                    } // Mod commands
-                    else if (msg.userInfo.isMod || msg.userInfo.isBroadcaster) {
-                        switch (message.trim()) {
-                            case "!startrace":
-                                startRace(socket, channel)
-                                break
-                            case "!stoprace":
-                                stopRace(socket, channel)
-                                break
-                            case "!mockdata":
-                                startMockData(socket, channel, 100)
-                                break
-                        }
-                    }
-                }
-            });
+            socket.on('requestMockData', (data2) => {
+                startMockData(data.channel, 100)
+            })
         }
     })
 })
 
-function syncData(socket, channel) {
-    socket.emit("channelData", channelData[channel])
+function syncData(channel, socket = null) {
+    if (socket) {
+        socket.emit("channelData", channelData[channel])
+    } else {
+        console.log(`syncing channel '${channel}' with ${channelClients[channel].length} clients`)
+        for (let client of channelClients[channel]) {
+            client.emit("channelData", channelData[channel])
+        }
+    }
 }
 
-function finishRace(socket, channel) {
+function finishRace(channel) {
     if (channelDataIntervals[channel]) {
         clearInterval(channelDataIntervals[channel])
         channelDataIntervals[channel] = null
     }
     if (channelData[channel].gameState != gameStates.Finished) {
         channelData[channel].gameState = gameStates.Finished
-        syncData(socket, channel)
+        syncData(channel)
         console.log("game over! the winner is:", channelData[channel].winner)
 
         setTimeout(() => {
             console.log("stopping finish screen")
-            stopRace(socket, channel)
+            stopRace(channel)
         }, config.server.RESET_DELAY)
     }
 }
 
-function stopRace(socket, channel) {
+function stopRace(channel) {
     if (channelDataIntervals[channel]) {
         clearInterval(channelDataIntervals[channel])
         channelDataIntervals[channel] = null
     }
-    channelData[channel].gameState = gameStates.Inactive
-    syncData(socket, channel)
+    clearLobsters(channel)
+    syncData(channel)
 }
 
-function startRace(socket, channel) {
+function startRace(channel) {
     if (!channelDataIntervals[channel] && channelData[channel].gameState == gameStates.Inactive) {
         channelData[channel].gameState = gameStates.Active
-        syncData(socket, channel)
+        syncData(channel)
         console.log("new game started")
 
         channelDataIntervals[channel] = setInterval(() => {
-            syncData(socket, channel)
+            syncData(channel)
         }, config.server.SYNC_DELAY)
     }
 }
 
-function addLobster(socket, channel, user, color = "dd2e44") {
+function addLobster(channel, user, color = "dd2e44") {
     if (channelData[channel]) {
         channelData[channel].lobsters[user] ??= {
             count: 0,
@@ -166,7 +191,7 @@ function addLobster(socket, channel, user, color = "dd2e44") {
         if (channelData[channel].lobsters[user].count++ >= channelData[channel].goal) {
             // game over
             channelData[channel].winner = user
-            finishRace(socket, channel)
+            finishRace(channel)
             console.log("game over! the winner is:", user)
         }
     } else {
@@ -183,26 +208,26 @@ function clearLobsters(channel) {
 }
 
 let mockActive = false;
-function startMockData(socket, channel, count) {
+function startMockData(channel, count) {
     if (!mockActive) {
         mockActive = true;
         clearLobsters(channel);
-        startRace(socket, channel)
+        startRace(channel)
 
         let i = 0;
         const interval = setInterval(() => {
             if (i++ == count) {
-                // stopRace(socket, channel)
+                // stopRace(channel)
                 return clearInterval(interval)
             } else if (channelData[channel].winner) {
                 return clearInterval(interval)
             } else {
-                if (Math.random() < 0.5) { addLobster(socket, channel, "aryu", Math.random() * 0xFFFFFF) }
-                if (Math.random() < 0.5) { addLobster(socket, channel, "risshella", Math.random() * 0xFFFFFF) }
-                if (Math.random() < 0.5) { addLobster(socket, channel, "kokos", Math.random() * 0xFFFFFF) }
-                if (Math.random() < 0.5) { addLobster(socket, channel, "wooloo", Math.random() * 0xFFFFFF) }
-                if (Math.random() < 0.5) { addLobster(socket, channel, "liv", Math.random() * 0xFFFFFF) }
-                if (Math.random() < 0.5) { addLobster(socket, channel, "reila", Math.random() * 0xFFFFFF) }
+                if (Math.random() < 0.5) { addLobster(channel, "aryu", Math.random() * 0xFFFFFF) }
+                if (Math.random() < 0.5) { addLobster(channel, "risshella", Math.random() * 0xFFFFFF) }
+                if (Math.random() < 0.5) { addLobster(channel, "kokos", Math.random() * 0xFFFFFF) }
+                if (Math.random() < 0.5) { addLobster(channel, "wooloo", Math.random() * 0xFFFFFF) }
+                if (Math.random() < 0.5) { addLobster(channel, "liv", Math.random() * 0xFFFFFF) }
+                if (Math.random() < 0.5) { addLobster(channel, "reila", Math.random() * 0xFFFFFF) }
             }
         }, config.server.SYNC_DELAY)
         mockActive = false;
@@ -211,8 +236,9 @@ function startMockData(socket, channel, count) {
 
 
 /* channelData structure
-[
+{
     "risshella": {
+        "clients": []
         "gameState": gameState.InProgress,
         "winner": null,
         "goal": 100,
@@ -230,8 +256,20 @@ function startMockData(socket, channel, count) {
         },
     }
     "aryu": {
-        "gameState": false,
-        "lobsters": []
+        ...
     }
-]
+}
+*/
+
+/* channelClients structure
+{
+    "risshella": [
+        client1,
+        client2
+    ],
+    "aryu": [
+        client3,
+        client4
+    ]
+}
 */
